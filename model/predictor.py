6e18345a917bd16b5a9bb168e481d000b0626b8f
FlewRr <cool.adsdad@mail.ru>
2023-06-09 18:29:40 +0300

    added everything

--- model/predictor.py
+++ model/predictor.py
@@ -1,4 +1,6 @@
 import torch
 
 model = torch.hub.load('pytorch/vision:v0.10.0', 'googlenet', pretrained=True)
-model.load_state_dict(torch.load('model', map_location=torch.device('cpu')))
+model.eval()
+model.fc = torch.nn.Linear(1024, 116)
+model.load_state_dict(torch.load('model/model_state', map_location=torch.device('cpu')))
